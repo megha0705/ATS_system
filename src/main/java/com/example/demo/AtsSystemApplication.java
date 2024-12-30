a26f61992b847da5a7aff2a44dfb93b1f51215c9
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2024-12-30 23:18:55 +0530

    Initial commit for Spring Initializer project

---
+++ src/main/java/com/example/demo/AtsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsSystemApplication.class, args);
+	}
+
+}
