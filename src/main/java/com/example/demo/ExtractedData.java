6a3e9baf2485d1403e30b45092c5fb36107fe014
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-01-12 21:32:45 +0530

    object for skill

---
+++ src/main/java/com/example/demo/ExtractedData.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class ExtractedData {
+    private String skills;
+    ExtractedData(){
+
+    }
+
+    public void setSkill(String skills){
+        this.skills = skills;
+    }
+    public String getSkill(){
+        return skills;
+    }
+}
