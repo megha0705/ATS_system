ab4931cb96adc69a6859fbbc3e73cafe53767c2b
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-01-14 23:09:15 +0530

    saving extracted data

--- src/main/java/com/example/demo/ServiceImp.java
+++ src/main/java/com/example/demo/ServiceImp.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceImp implements service {
-       @Autowired
+    @Autowired
     CandidateRepo repo;
-
+    @Autowired
+    ExtractedDataRepo exRepo;
+    public ServiceImp() {
+        System.out.println("ServiceImp Constructor - Beans Injected");
+    }
     public void candidateDetail(String name , String email){
             CandidateEntity entity = new CandidateEntity();
             entity.setEmailAddress(email);
@@ -15,4 +19,23 @@ public class ServiceImp implements service {
             repo.save(entity);
            
     }
+
+@Override
+public void ExtractedDataResume(String skills, String experience, String projects) {
+        // TODO Auto-generated method stub
+       // throw new UnsupportedOperationException("Unimplemented method 'ExtractedDataResume'");
+       if(exRepo != null){
+        System.out.println("ExtractedDataRepo injected correctly!");
+       }else{
+        System.out.println("error injecting");
+       }
+ExtractedDataEntity entity = new ExtractedDataEntity();
+entity.setExperience(experience);
+entity.setProjects(projects);
+entity.setSkill(skills);
+exRepo.save(entity); 
+
+}
+
+   
 }
