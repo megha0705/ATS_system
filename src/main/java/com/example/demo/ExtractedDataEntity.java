001a8b8bffdd3638806dc815cd6f5f4a7cef7c0e
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-01-16 22:48:52 +0530

    column definition added

--- src/main/java/com/example/demo/ExtractedDataEntity.java
+++ src/main/java/com/example/demo/ExtractedDataEntity.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,8 +17,11 @@ public class ExtractedDataEntity {
     @OneToOne
     @JoinColumn(name = "candidate_id", unique = true) 
     private CandidateEntity can_id;
+     @Column( columnDefinition = "TEXT") 
     private String skills;
+    @Column( columnDefinition = "TEXT") 
     private String experience;
+    @Column( columnDefinition = "TEXT") 
     private String project;
     ExtractedDataEntity(){
     }
