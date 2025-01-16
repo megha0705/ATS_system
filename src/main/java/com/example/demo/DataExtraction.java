1e02ed7e24d0b2d063d2556e804a9ec541bcb283
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-01-16 22:47:52 +0530

    autowired added

--- src/main/java/com/example/demo/DataExtraction.java
+++ src/main/java/com/example/demo/DataExtraction.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 @Service
 public class DataExtraction {
       @Autowired
@@ -20,7 +21,7 @@ public class DataExtraction {
                 int j = i;
               
                 while(j < str1.length && !(str1[j].trim().equals("experience")) && !(str1[j].trim().equals("projects")) && !(str1[j].trim().equals("education")) &&  !(str1[j].trim().equals("certification")) ){
-                    skill.append(str1[j]).append("\n");
+                    skill.append(str1[j]).append(" ");
                     j++;
                 }
                 
@@ -29,7 +30,7 @@ public class DataExtraction {
                 int j = i;
                 
                 while(j < str1.length && !(str1[j].trim().equals("skills")) && !(str1[j].trim().equals("projects")) &&!(str1[j].trim().equals("certifications")) && !(str1[j].trim().equals("education")) ){
-                    experience.append(str1[j]).append("\n");
+                    experience.append(str1[j]).append(" ");
                     j++;
                 }
               
@@ -37,7 +38,7 @@ public class DataExtraction {
             }else if(str1[i].trim().equals("projects")){
                 int j = i;
                 while(j < str1.length && !(str1[j].trim().equals("skills")) && !(str1[j].trim().equals("experience")) && !(str1[j].trim().equals("certifications")) && !(str1[j].trim().equals("education")) ){
-                    projects.append(str1[j]).append("\n");
+                    projects.append(str1[j]).append(" ");
                     j++;
                 }
             
@@ -49,7 +50,7 @@ public class DataExtraction {
             s.ExtractedDataResume(skill.toString(), experience.toString(), projects.toString());
 
         }else{
-            s.ExtractedDataResume(skill.toString(), experience.toString(), projects.toString());
+           
 
             System.out.println("The object 's' is null.");
         }
