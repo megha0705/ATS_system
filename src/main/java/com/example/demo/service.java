f6e7deb7ba94bf319c8cb83c02a84517f7631b73
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-01-17 21:08:52 +0530

    added method for job requirement

--- src/main/java/com/example/demo/service.java
+++ src/main/java/com/example/demo/service.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
+
 public interface service {
     void candidateDetail(String name , String email);
+    void ExtractedDataResume(String skills, String experience , String projects);
+    void jobRequirementDetails(String jobTitle , String skills, int experience);
+    
 }
 
