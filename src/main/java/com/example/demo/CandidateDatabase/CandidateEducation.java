8610e56cb36876efc3567052e60a80c3a76eb1bb
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-01-23 13:53:36 +0530

    entity for candidate education

---
+++ src/main/java/com/example/demo/CandidateDatabase/CandidateEducation.java
@@ -0,0 +1,21 @@
+package com.example.demo.CandidateDatabase;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "education_details_of_candidate")
+public class CandidateEducation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int edu_id;
+  @OneToOne
+  @JoinColumn(name = "candidate_id", unique = true) 
+  private PersonalDetails candidate_id;
+  private String degree_name;
+  private String edu_institution_name;
+  private int year_of_passing;  
+}
