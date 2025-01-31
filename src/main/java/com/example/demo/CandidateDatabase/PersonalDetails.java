907bee4be359fb6e1dfaf6ac32b9d06fb59048a6
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-01-31 15:19:28 +0530

    added another property for candidateSkills

--- src/main/java/com/example/demo/CandidateDatabase/PersonalDetails.java
+++ src/main/java/com/example/demo/CandidateDatabase/PersonalDetails.java
@@ -1,9 +1,13 @@
 package com.example.demo.CandidateDatabase;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,9 +20,11 @@ import lombok.NoArgsConstructor;
 public class PersonalDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
     private int candidate_id;
     private String candidate_name;
     private String candidate_email;
-    
+    @OneToMany(mappedBy = "candidateId", cascade = CascadeType.ALL)
+    private List<CandidateSkills> candidateSkills;
 
 }
