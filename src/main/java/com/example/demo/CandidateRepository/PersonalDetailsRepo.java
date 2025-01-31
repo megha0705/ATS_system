a63f7e6618f1ba5dd7e49d1aabe733a145d0fa9c
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-01-31 23:24:43 +0530

    created query for matching the data

--- src/main/java/com/example/demo/CandidateRepository/PersonalDetailsRepo.java
+++ src/main/java/com/example/demo/CandidateRepository/PersonalDetailsRepo.java
@@ -1,9 +1,14 @@
 package com.example.demo.CandidateRepository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.demo.CandidateDatabase.PersonalDetails;
 @Repository
 public interface PersonalDetailsRepo extends JpaRepository<PersonalDetails, Integer> {
-
+    @Query("SELECT p.candidate_name FROM PersonalDetails p JOIN p.candidateExperience e JOIN p.candidateSkills s JOIN p.candidateEducation ed WHERE e.job_title = :job_title AND e.months_of_experience = :months_of_experience AND s.skillName IN :skillName AND ed.degree_name = :degree_name")
+    List<String> findByCandidateExperienceAndSkillsAndEducation(@Param("job_title") String job_title ,@Param("months_of_experience") double months_of_experience, @Param("skillName") List<String> skillName, @Param("degree_name") String degree_name);
 }
