40426296cb563bb8a6c197aaa290c0b8d7825682
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-01-31 23:29:54 +0530

    modification of the functions

--- src/main/java/com/example/demo/service.java
+++ src/main/java/com/example/demo/service.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-
-
+import java.util.ArrayList;
+import java.util.*;
 public interface service {
     void candidateDetail(String name , String email);
-    void candidateSkills(String skills);
+    void candidateSkills(ArrayList<String> skills);
     void candidateExperience(String job_title,String company_name, double months_of_experience);
-    void candidateEducation(String degree_name);
-
-     //void jobRequirementDetails(String jobTitle , String skills, int experience);
+    void candidateEducation(ArrayList<String> degree_name);
+    List<String> jobRequirementDetails(String jobTitle , String skills, int experience, String degree);
+     
 }
 
