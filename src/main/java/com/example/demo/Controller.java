ebeb18757a6aca26195b23c6e7bd2dcd09dfae81
Reshmi Chakraborty <chakrabortyreshmi077@gmail.com>
2025-01-31 23:29:14 +0530

    returning candidate name list

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,28 +11,35 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.example.demo.ResumeHandling.ResumeStoring;
+
 @RestController
 public class Controller {
 
     @Autowired
     service s;
     
+   // @Autowired
+    //PdfHandler pdf;
     @Autowired
-    PdfHandler pdf;
-
+    ResumeStoring resume_store;
     @PostMapping("ats")
     public RedirectView atsSystem(@RequestParam String name , @RequestParam String email, @RequestParam MultipartFile file) throws Exception{
         s.candidateDetail(name, email);
-        pdf.saveResume(file) ;
-        return new RedirectView("/jobRequirementForm.html");
+        resume_store.saveResume(file) ;
+       return  new RedirectView("/formSubmission.html");
      
     }
+ 
     @PostMapping("submitJobRequirement")
-    public String submitRequirementForm(@RequestParam String jobTitle , @RequestParam String skills, @RequestParam int experience ){
-        s.jobRequirementDetails(jobTitle, skills, experience);
-        
-        return "data submitted succesfully";
+    public List<String> submitRequirementForm(@RequestParam String jobTitle , @RequestParam String skills, @RequestParam int experience, @RequestParam String degree ){
+     List<String> candidate_name =   s.jobRequirementDetails(jobTitle, skills, experience,degree);
+    //    return new RedirectView("/candidateListing.html");
+    return candidate_name;
+       
 
     }
     
 }
+//RedirectView 
+//return new RedirectView("/jobRequirementForm.html");
